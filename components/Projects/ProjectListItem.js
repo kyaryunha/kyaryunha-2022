b6cebc10ac23de423a9f6fae731978afd6beb7f6
kyaryunha <kyaryunha@naver.com>
2022-06-02 03:24:59 +0900

    update project with card view

--- components/Projects/ProjectListItem.js
+++ components/Projects/ProjectListItem.js
@@ -1,9 +1,32 @@
-import {StyledProjectListItem, StyledProjectListItemTitle} from "../../styles/Projects.style";
+import {
+    StyledProjectImage,
+    StyledProjectListItem,
+    StyledProjectListItemContents,
+    StyledProjectListItemDate,
+    StyledProjectListItemTitle
+} from "../../styles/Projects.style";
+import {IconLink} from "../../icons/util.icon";
+import {kyaColor} from "../../styles/Layout.style";
+import {KyaA} from "../../styles/Page.style";
 
-export default function ProjectListItem ({title}) {
+export default function ProjectListItem ({date, title, contents, coverImageUrl, url}) {
     return <StyledProjectListItem>
         <StyledProjectListItemTitle>
-            {title}
+            <StyledProjectListItemDate>
+                {date}
+            </StyledProjectListItemDate>
+            {
+                url ?
+                        <KyaA href={url} target='_blank'>
+                            {title}
+                            <IconLink />
+                        </KyaA>
+                    : title
+            }
         </StyledProjectListItemTitle>
+        <StyledProjectImage src={coverImageUrl} alt="my picture" />
+        <StyledProjectListItemContents>
+            {contents}
+        </StyledProjectListItemContents>
     </StyledProjectListItem>
 }
